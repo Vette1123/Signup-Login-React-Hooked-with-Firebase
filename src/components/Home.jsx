ad23dd694e6a5cd179734b86bed55438cf3df571
MohamedGado <boogado@yahoo.com>
2022-04-01 10:29:36 +0200

    added profile page to change pw, single reg/signup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import "../styles/Home.module.css";
 import { NavLink } from "react-router-dom";
+import AuthContext from "../storeTokens/Auth-Context";
 
 function Home() {
+  const authCtx = useContext(AuthContext);
+  const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
   return (
     <>
       <header>
@@ -11,12 +18,26 @@ function Home() {
             <li className="first">
               <NavLink to="/">Home</NavLink>
             </li>
-            <li>
-              <NavLink to="/signup">Signup</NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
+            {!isLoggedIn && (
+              <li>
+                <NavLink to="/auth">Signup/Login</NavLink>
+              </li>
+            )}
+            {isLoggedIn && (
+              <li>
+                <NavLink to="/profile">Profile</NavLink>
+              </li>
+            )}
+            {/* {!isLoggedIn && (
+              <li>
+                <NavLink to="/login">Login</NavLink>
+              </li>
+            )} */}
+            {isLoggedIn && (
+              <li>
+                <button onClick={logoutHandler}>Logout</button>
+              </li>
+            )}
           </ul>
         </div>
       </header>
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import classes from "../styles/SignUpForm.module.css";
 import useInput from "../hooks/use-input";
 import FormGroup from "./FormGroup";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../storeTokens/Auth-Context";
+import axios from "axios";
 
 const emailRegex =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -29,7 +31,7 @@ const validateEmail = (val) => {
   return { isValid: true };
 };
 
-const validatePassword = (val) => {
+export const validatePassword = (val) => {
   let password = val.toString().trim().toLowerCase();
   if (password === "") return { isValid: false, msg: "Password is required" };
   if (password.length < 8)
@@ -41,6 +43,10 @@ const validatePassword = (val) => {
 };
 
 const SignUpForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const authCtx = useContext(AuthContext);
+  let navigate = useNavigate();
+
   const {
     enteredValue: enteredName,
     hasError: nameInputHasError,
@@ -89,6 +95,9 @@ const SignUpForm = () => {
     resetHandler: conresetPasswordHandler,
   } = useInput(validateconPassword);
 
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
   const onSumbitHandler = (e) => {
     e.preventDefault();
     console.log("Form is sumbitted successfully");
@@ -96,20 +105,106 @@ const SignUpForm = () => {
     resetEmailHandler();
     resetPasswordHandler();
     conresetPasswordHandler();
+
+    if (isLogin) {
+      axios
+        .post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCzTfDrGGGFjKW3KWnQHVSW6nq7P-F3DXU",
+          {
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }
+        )
+        .then((res) => {
+          authCtx.login(
+            res.data.idToken,
+            new Date(Date.now() + res.data.expiresIn * 1000)
+          );
+          navigate("/");
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    } else {
+      axios
+        .post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCzTfDrGGGFjKW3KWnQHVSW6nq7P-F3DXU",
+          {
+            email: enteredEmail,
+            password: enteredPassword,
+            name: enteredName,
+            returnSecureToken: true,
+          }
+        )
+        .then((res) => {
+          authCtx.login(
+            res.data.idToken,
+            new Date(Date.now() + res.data.expiresIn * 1000)
+          );
+          navigate("/");
+        })
+        .catch((err) => {
+          alert(err.message);
+          let errorMessage = "Authentication failed!";
+          throw new Error(errorMessage);
+        });
+
+      // fetch(
+      //   "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCzTfDrGGGFjKW3KWnQHVSW6nq7P-F3DXU",
+      //   {
+      //     method: "POST",
+      //     body: JSON.stringify({
+      //       email: enteredEmail,
+      //       password: enteredPassword,
+      //       returnSecureToken: true,
+      //     }),
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //     },
+      //   }
+      // )
+      //   .then((res) => {
+      //     if (res.ok) {
+      //       return res.json();
+      //     } else {
+      //       return res.json().then((data) => {
+      //         let errorMessage = "Authentication failed!";
+      //         throw new Error(errorMessage);
+      //       });
+      //     }
+      //   })
+      //   .then((data) => {
+      //     authCtx.login(
+      //       data.idToken,
+      //       new Date(Date.now() + data.expiresIn * 1000)
+      //     );
+      //     // useNavigate("/");
+      //     // history.replace("/");
+      //   })
+      //   .catch((err) => {
+      //     alert(err.message);
+      //   });
+    }
   };
 
   return (
     <form onSubmit={onSumbitHandler} className={classes.form}>
-      <h1>Sign Up Form</h1>
-      <FormGroup
-        type="text"
-        name="name"
-        valueChangedHandler={nameChangeHandler}
-        inputBlurHandler={nameBlurHandler}
-        enteredValue={enteredName}
-        inputHasError={nameInputHasError}
-        errMsg={nameErrMsg}
-      />
+      <h1>{isLogin ? "Log in Form" : "Sign Up Form"}</h1>
+
+      {!isLogin ? (
+        <FormGroup
+          type="text"
+          name="name"
+          valueChangedHandler={nameChangeHandler}
+          inputBlurHandler={nameBlurHandler}
+          enteredValue={enteredName}
+          inputHasError={nameInputHasError}
+          errMsg={nameErrMsg}
+        />
+      ) : (
+        ""
+      )}
       <FormGroup
         type="text"
         name="email"
@@ -119,6 +214,7 @@ const SignUpForm = () => {
         inputHasError={emailInputHasError}
         errMsg={emailErrMsg}
       />
+
       <FormGroup
         type="password"
         name="password"
@@ -128,31 +224,54 @@ const SignUpForm = () => {
         inputHasError={passwordInputHasError}
         errMsg={passwordErrMsg}
       />
-
-      <FormGroup
-        type="password"
-        name="Confim Password"
-        valueChangedHandler={conpasswordChangeHandler}
-        inputBlurHandler={conpasswordBlurHandler}
-        enteredValue={conenteredPassword}
-        inputHasError={conpasswordInputHasError}
-        errMsg={conpasswordErrMsg}
-      />
-      <p>
-        Already registerd?
-        <Link to="/login"> Login!</Link>
-      </p>
+      {!isLogin ? (
+        <FormGroup
+          type="password"
+          name="Confim Password"
+          valueChangedHandler={conpasswordChangeHandler}
+          inputBlurHandler={conpasswordBlurHandler}
+          enteredValue={conenteredPassword}
+          inputHasError={conpasswordInputHasError}
+          errMsg={conpasswordErrMsg}
+        />
+      ) : (
+        ""
+      )}
+      <div>
+        {isLogin ? "Not a registerd user?" : "Already a registered user?"}
+        {/* <Link to="/login"> Login!</Link> */}
+        <button
+          onClick={switchAuthModeHandler}
+          type="button"
+          className={classes.toggle}
+        >
+          {isLogin ? "Register" : "Login!"}
+        </button>
+      </div>
 
       <div className={classes["form-action"]}>
-        <button
-          className={`${
-            isEmailValid && isNameValid && isPasswordValid && conisPasswordValid
-              ? ""
-              : classes.disabled
-          }`}
-        >
-          SignUp
-        </button>
+        {isLogin ? (
+          <button
+            className={`${
+              isEmailValid && isPasswordValid ? "" : classes.disabled
+            }`}
+          >
+            {isLogin ? "Login" : "SignUp"}
+          </button>
+        ) : (
+          <button
+            className={`${
+              isEmailValid &&
+              isNameValid &&
+              isPasswordValid &&
+              conisPasswordValid
+                ? ""
+                : classes.disabled
+            }`}
+          >
+            {isLogin ? "Login" : "SignUp"}
+          </button>
+        )}
       </div>
     </form>
   );
